bb4dddc37fd0dbfc4c844bc302cc2f95beeb4752
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-03-14 20:51:17 +0530

    14th-march-contest-2208

---
+++ CODEFORCES/2208/2208A.cpp
@@ -0,0 +1,90 @@
+/**
+ * Author: KUNWAR
+ * Date:   2026-03-14
+ * Time:   20:10:03
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Definitions ---
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using ld = long double;
+
+// --- Macros ---
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+#define fkv(k, v, m) for(auto &[k, v] : m)
+#define all(x) (x).begin(), (x).end()
+#define get_sum(v, total) for(auto &x : v) total += x
+#define cn continue
+#define op(x) cout << x << endl
+
+// --- Input Macros ---
+#define read(...) __VA_ARGS__; read_input(__VA_ARGS__)
+void read_input() {}
+template<typename T, typename... Args>
+void read_input(T& first, Args&... rest) { cin >> first; read_input(rest...); }
+#define var(type, ...) type __VA_ARGS__; read_input(__VA_ARGS__)
+#define i(x) int x; cin >> x
+#define l(x) long long x; cin >> x
+#define s(x) string x; cin >> x
+#define c(x) char x; cin >> x
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+bool solve() {
+    l(n);
+    if(n == 1)
+    {
+        l(val);
+        return false;
+    }
+    vvll a(n , vll(n, 0));
+
+    f(i , 0 , n)
+    {
+        f(j , 0 , n)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    vll freq(n*n+1 , 0) ;
+    f(i , 0 , n)
+    {
+        f(j , 0 , n)
+        {
+            freq[a[i][j]]++ ;
+            if(freq[a[i][j]] > n * (n-1)) 
+            {
+                return false ;
+            }
+        }
+    } 
+    return true;
+}
+
+int main() {
+    fastio();
+    l(t);
+    while(t--) {
+        if(solve()) cout << "YES" << endl;
+        else cout << "NO" << endl ;
+    }
+    return 0;
+}
---
+++ CODEFORCES/2208/2208B.cpp
@@ -0,0 +1,113 @@
+/**
+ * Author: KUNWAR
+ * Date:   2026-03-14
+ * Time:   20:22:05
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Definitions ---
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using ld = long double;
+
+// --- Macros ---
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+#define fkv(k, v, m) for(auto &[k, v] : m)
+#define all(x) (x).begin(), (x).end()
+#define get_sum(v, total) for(auto &x : v) total += x
+#define cn continue
+#define op(x) cout << x << endl
+
+// --- Input Macros ---
+#define read(...) __VA_ARGS__; read_input(__VA_ARGS__)
+void read_input() {}
+template<typename T, typename... Args>
+void read_input(T& first, Args&... rest) { cin >> first; read_input(rest...); }
+#define var(type, ...) type __VA_ARGS__; read_input(__VA_ARGS__)
+#define i(x) int x; cin >> x
+#define l(x) long long x; cin >> x
+#define s(x) string x; cin >> x
+#define c(x) char x; cin >> x
+
+template <typename T> vector<T> in(int n) { vector<T> v(n); for (T &x : v) cin >> x; return v; }
+template <typename T> void out(const vector<T> &v) { for (const T &x : v) cout << x << ' '; cout << '\n'; }
+template<typename T> vector<vector<T>> create2D(size_t rows, size_t cols, T default_value = T{}) { return vector<vector<T>>(rows, vector<T>(cols, default_value)); }
+template <typename T> vector<vector<T>> in2D(int r, int c) { auto v = create2D<T>(r, c); for (auto &row : v) for (T &cell : row) cin >> cell; return v; }
+template<typename T> void out2D(const vector<vector<T>>& vec) { for (size_t i = 0; i < vec.size(); ++i) { for (size_t j = 0; j < vec[i].size(); ++j) { cout << vec[i][j] << (j < vec[i].size() - 1 ? " " : ""); } cout << "\n"; } }
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+ll solve() {
+    l(n);
+    l(k);
+    l(p);
+    l(m);
+    auto a = in<ll>(n);
+    ll cst = 0 ; 
+    if(p <= k) cst = 0 ; 
+    else 
+    {
+        vll card(a.begin() , a.begin() + p - 1);;
+        sort(all(card));
+        f(i , 0 , p-k)
+        {
+            cst += card[i]; 
+        }
+    }
+    ll win = a[p-1];
+    
+    if(m < cst + win) 
+    {
+        return 0;
+    }
+
+    m -= (cst + win);
+    ll ans = 1;
+
+    ll cyclic = win;
+    
+    if(k < n) 
+    {
+        vll remain;
+        f(i , 0 , n) 
+        {
+            if(i != p - 1) 
+            {
+                remain.push_back(a[i]);
+            }
+        }
+        sort(all(remain));
+        f(i , 0 , n - k) {
+            cyclic += remain[i];
+        }
+    }
+
+    ans += (m / cyclic);
+    return ans ;
+}
+
+int main() {
+    fastio();
+    l(t);
+    while(t--) {
+        ll ans = solve();
+        cout << ans << endl;
+    }
+    return 0;
+}
