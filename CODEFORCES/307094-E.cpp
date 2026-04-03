8594f20707e1790268abdf43fbba80a390f47a9f
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-04-04 04:40:52 +0530

    4th-april-2026

---
+++ CODEFORCES/307094-E.cpp
@@ -0,0 +1,101 @@
+// https://codeforces.com/edu/course/2/lesson/9/3/practice/contest/307094/problem/E
+
+/**
+ * Author: KUNWAR
+ * Date:   2026-04-04
+ * Time:   03:42:49
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Definitions ---
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using ld = long double;
+
+// --- Macros ---
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+#define fkv(k, v, m) for(auto &[k, v] : m)
+#define all(x) (x).begin(), (x).end()
+#define get_sum(v, total) for(auto &x : v) total += x
+#define cn continue
+#define op(x) cout << x << endl
+
+// --- Input Macros ---
+#define read(...) __VA_ARGS__; read_input(__VA_ARGS__)
+void read_input() {}
+template<typename T, typename... Args>
+void read_input(T& first, Args&... rest) { cin >> first; read_input(rest...); }
+#define var(type, ...) type __VA_ARGS__; read_input(__VA_ARGS__)
+#define i(x) int x; cin >> x
+#define l(x) long long x; cin >> x
+#define s(x) string x; cin >> x
+#define c(x) char x; cin >> x
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+template <typename T> vector<T> in(int n) { vector<T> v(n); for (T &x : v) cin >> x; return v; }
+template <typename T> void out(const vector<T> &v) { for (const T &x : v) cout << x << ' '; cout << '\n'; }
+template<typename T> vector<vector<T>> create2D(size_t rows, size_t cols, T default_value = T{}) { return vector<vector<T>>(rows, vector<T>(cols, default_value)); }
+template <typename T> vector<vector<T>> in2D(int r, int c) { auto v = create2D<T>(r, c); for (auto &row : v) for (T &cell : row) cin >> cell; return v; }
+template<typename T> void out2D(const vector<vector<T>>& vec) { for (size_t i = 0; i < vec.size(); ++i) { for (size_t j = 0; j < vec[i].size(); ++j) { cout << vec[i][j] << (j < vec[i].size() - 1 ? " " : ""); } cout << "\n"; } }
+
+void solve() {
+    l(n);
+    l(s);
+    auto w = in<ll>(n);
+    auto c = in<ll>(n);
+
+    ll left = 0 ; 
+    ll right = 0 ;
+
+    vll wp(n+1 , 0);
+    vll cp(n+1 , 0);
+
+    f(i , 1 , n+1)
+    {
+        wp[i] = w[i-1] + wp[i-1];
+        cp[i] = c[i-1] + cp[i-1];
+    }
+
+    ll ps = -1 ;
+
+    while(right <= n)
+    {
+        sum += a[right];
+        while(left <= right && wp[right] - wp[left] > s)
+        {
+            sum -= a[left];
+            left++ ; 
+        }
+        if((wp[right] - wp[left]) <= s && ps < (cp[right] - cp[left])) ps = cp[right] - cp[left];
+        right++ ;
+    }
+
+    cout << ps << endl;
+}
+
+int main() {
+    fastio();
+    // l(t);
+    ll t = 1 ;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
