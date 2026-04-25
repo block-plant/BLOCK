1c14ee1e811e2d2226727c3c7ba32072e2319c77
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-04-25 20:30:01 +0530

    25th-april-2026

---
+++ CODEFORCES/2222/2222A.cpp
@@ -0,0 +1,58 @@
+/**
+ * Author: Ayush Kunwar Singh
+ * Time:   20:07:49
+ * Date:   2026-04-25
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+#define all(x) (x).begin(), (x).end()
+#define i(x) int x ; cin >> x
+#define l(x) long long x; cin >> x
+#define op(x) cout << x << endl
+
+void fastio() { ios_base::sync_with_stdio(false); cin.tie(NULL); }
+
+template <typename T> vector<T> in(int n) { vector<T> v(n); for (T &x : v) cin >> x; return v; }
+template <typename T> void out(const vector<T> &v) { for (const T &x : v) cout << x << ' '; cout << '\n'; }
+template<typename T> vector<vector<T>> create2D(size_t rows, size_t cols, T default_value = T{}) { return vector<vector<T>>(rows, vector<T>(cols, default_value)); }
+template <typename T> vector<vector<T>> in2D(int r, int c) { auto v = create2D<T>(r, c); for (auto &row : v) for (T &cell : row) cin >> cell; return v; }
+template<typename T> void out2D(const vector<vector<T>>& vec) { if(vec.empty() || vec[0].empty()) return; for (size_t i = 0; i < vec.size(); ++i) { for (size_t j = 0; j < vec[i].size(); ++j) cout << vec[i][j] << (j < vec[i].size() - 1 ? " " : ""); cout << "\n"; } }
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a == 0 || b == 0) ? 0 : (a / gcd(a, b)) * b; }
+ll binpow(ll base, ll exp, ll mod) { ll res = 1; base %= mod; while(exp > 0) { if(exp & 1) res = (res * base) % mod; base = (base * base) % mod; exp >>= 1; } return res; }
+
+void solve() {
+    l(n);
+    auto a = in<ll>(n);
+    
+    bool p = false;
+    f(i , 0 , n) {
+        if(a[i] == 100) {
+            p = true;
+            break;
+        }
+    }
+
+    if(p) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    fastio();
+    l(t);
+    while(t--) solve();
+    return 0;
+}
