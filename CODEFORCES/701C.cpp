b571ba062f5fee27e031095be85b9c34b994c192
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-05-24 12:40:33 +0530

    24th-may-2026

---
+++ CODEFORCES/701C.cpp
@@ -0,0 +1,85 @@
+/**
+ * Author: KUNWAR
+ * Date:   2026-05-24
+ * Time:   12:21:00
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Definitions ---
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using ld = long double;
+
+// --- Macros ---
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+#define fkv(k, v, m) for(auto &[k, v] : m)
+#define all(x) (x).begin(), (x).end()
+#define get_sum(v, total) for(auto &x : v) total += x
+#define cn continue
+#define op(x) cout << x << endl
+
+// --- Input Macros ---
+#define read(...) __VA_ARGS__; read_input(__VA_ARGS__)
+void read_input() {}
+template<typename T, typename... Args>
+void read_input(T& first, Args&... rest) { cin >> first; read_input(rest...); }
+#define var(type, ...) type __VA_ARGS__; read_input(__VA_ARGS__)
+#define i(x) int x; cin >> x
+#define l(x) long long x; cin >> x
+#define s(x) string x; cin >> x
+#define c(x) char x; cin >> x
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    l(n);
+    string s;
+    cin >> s;
+
+    map<char,ll> cnt;
+    f(i , 0 , n) {
+        cnt[s[i]]++ ;
+    }
+
+    ll sz = cnt.size() ;
+    ll ans = LLONG_MAX;
+    map<char , ll> mp ; 
+    for(int left = 0 , right = 0 ; right < n ; right++) {
+        mp[s[right]]++ ; 
+        while(left <= right && mp.size() == sz) {
+            ans = min(ans , (ll)(right - left + 1));
+            mp[s[left]]-- ;
+            if(mp[s[left]] == 0) {
+                mp.erase(s[left]);
+            }
+            left++ ;
+        }
+    }
+    op(ans);
+}
+
+int main() {
+    fastio();
+    // l(t);
+    ll t = 1 ;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
