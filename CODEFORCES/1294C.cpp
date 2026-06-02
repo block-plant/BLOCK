5f7c5b2273b2b554bad6ebfd1ecf29ae4062518f
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-06-03 00:32:49 +0530

    2nd-june-2026

---
+++ CODEFORCES/1294C.cpp
@@ -0,0 +1,86 @@
+/**
+ * Author: KUNWAR
+ * Date:   2026-06-03
+ * Time:   00:09:36
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Definitions ---
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using ld = long double;
+
+// --- Macros ---
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+#define fkv(k, v, m) for(auto &[k, v] : m)
+#define all(x) (x).begin(), (x).end()
+#define get_sum(v, total) for(auto &x : v) total += x
+#define cn continue
+#define op(x) cout << x << endl
+
+// --- Input Macros ---
+#define read(...) __VA_ARGS__; read_input(__VA_ARGS__)
+void read_input() {}
+template<typename T, typename... Args>
+void read_input(T& first, Args&... rest) { cin >> first; read_input(rest...); }
+#define var(type, ...) type __VA_ARGS__; read_input(__VA_ARGS__)
+#define i(x) int x; cin >> x
+#define l(x) long long x; cin >> x
+#define s(x) string x; cin >> x
+#define c(x) char x; cin >> x
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    l(n);
+    ll a = -1 , b = -1 , c = -1 ;
+    bool done = false ;
+    for(int i = 2 ; i * i <= n ; i++) {
+        if(n % i == 0) {
+            a = i ;
+            ll rem = n / i ; 
+            for(int j = i + 1 ; j * j <= n ; j++) {
+                if(rem % j == 0) {
+                    b = j ; 
+                    c = rem / j ;
+                    if(c > b) {
+                        done = true;
+                        break ;
+                    }
+                }
+            }
+            if(done) break;
+        }
+    }
+
+    if(a == -1 || b == -1 || c == -1 || (a == b) || (b == c) || (a == c)) cout << "NO" << endl;
+    else {
+        cout << "YES" << endl;
+        cout << a << ' ' << b << ' ' << c << endl ;
+    }
+}
+
+int main() {
+    fastio();
+    l(t);
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
