d5c996ae600ac2ae96d1545a7f44feef8ee7a4c5
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-06-12 23:58:16 +0530

    12th-June-2026

---
+++ CODEFORCES/2236/2236C.cpp
@@ -0,0 +1,88 @@
+/**
+ * Author: KUNWAR
+ * Date:   2026-06-12
+ * Time:   20:34:24
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Definitions ---
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using ld = long double;
+
+// --- Macros ---
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+#define fkv(k, v, m) for(auto &[k, v] : m)
+#define all(x) (x).begin(), (x).end()
+#define get_sum(v, total) for(auto &x : v) total += x
+#define cn continue
+#define op(x) cout << x << endl
+
+// --- Input Macros ---
+#define read(...) __VA_ARGS__; read_input(__VA_ARGS__)
+void read_input() {}
+template<typename T, typename... Args>
+void read_input(T& first, Args&... rest) { cin >> first; read_input(rest...); }
+#define var(type, ...) type __VA_ARGS__; read_input(__VA_ARGS__)
+#define i(x) int x; cin >> x
+#define l(x) long long x; cin >> x
+#define s(x) string x; cin >> x
+#define c(x) char x; cin >> x
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    l(a);
+    l(b);
+    l(x);
+
+    vll as ;
+    ll ta = a;
+    while(1) {
+        as.push_back(ta);
+        if(ta == 0) break; 
+        ta = ta / x ; 
+    }
+
+    vll bs ;
+    ll tb = b ; 
+    while(1) {
+        bs.push_back(tb);
+        if(tb == 0) break;
+        tb = tb / x ;
+    }
+
+    ll ans = LLONG_MAX ;
+
+    f(i , 0 , as.size()) {
+        f(j , 0 , bs.size()) {
+            ans = min(ans , (ll)(i + j + abs(as[i] - bs[j])));
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fastio();
+    l(t);
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
