2185b6ea0999fcad2bc01587dd8bb0885b4875d2
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-06-20 01:31:19 +0530

    19th-June-2026

--- CODEFORCES/1350B.cpp
+++ CODEFORCES/1350B.cpp
@@ -16,29 +16,30 @@ int main() {
 	        }
 	    }
 	    
-	    auto lis = [&](vector<ll>&b)->ll {
-            vector<ll> temp ;
-            for(auto x : b) {
-                auto it = lower_bound(temp.begin(), temp.end(), x);  
-                if(it == temp.end()) {
-                    temp.push_back(x);
-                }
-                else {
-                    *it = x ;
-                }
-            }
-            return temp.size();
-	    };
+	    // auto lis = [&](vector<ll>&b)->vector<ll> {
+        //     vector<ll> temp ;
+        //     for(auto x : b) {
+        //         auto it = lower_bound(temp.begin(), temp.end(), x);  
+        //         if(it == temp.end()) {
+        //             temp.push_back(x);
+        //         }
+        //         else {
+        //             *it = x ;
+        //         }
+        //     }
+        //     return temp;
+	    // };
 	    
 	    vector<ll> ans(n + 1 , 0);
 	    ll Ans = 0 ;
 	    for(int i = 1 ; i <= n ; i++) {
 	       // cout << i << ' ' << Ans << endl;
-	        vector<ll> divi;
-	        for(auto x : divs[i]) {
-	            divi.push_back(a[x]);
-	        }
-	        Ans = max(Ans , lis(divi));
+            for(auto j : divs[i]) {
+                if(j < i && a[j] < a[i]) {
+                    ans[i] = max(ans[i] , ans[j] + 1);
+                }
+            }
+	        Ans = max(Ans , ans[i]);
 	    }
 	    cout << Ans << endl;
 	}
