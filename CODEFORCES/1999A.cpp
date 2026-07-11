f6b8f994f442dc960fe5a8e696dd080a9a2bc8e0
block-plant <225452759+block-plant@users.noreply.github.com.>
2026-07-12 00:00:30 +0530

    11th-July-2026

---
+++ CODEFORCES/1999A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string s; 
+    cin >> s ;
+    cout << (s[0] - '0') + (s[1] - '0') << endl;
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+}
